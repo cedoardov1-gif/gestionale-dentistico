6ae9c7ad50da1fd9c12de47d80cc5dda56c0b5e5
Edoardo Cessari <c.edoardo.v1@gmail.com>
2026-04-20 23:44:17 +0200

    fix supabase credentials

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+export const supabase = createClient(
+  'https://qzbrzqokeilypnhgmzeq.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6YnJ6cW9rZWlseXBuaGdtemVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MjEwNjgsImV4cCI6MjA5MTM5NzA2OH0.0xJ0F7rJ0e9ph7Ga1c9pEW4otZbGcmYpogRFutZI4Ak'
+)
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
